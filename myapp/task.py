4799993a3579b7e3630d085023f5eb4b2dc98f2f
JianingGeng <113857144+JianingGeng@users.noreply.github.com>
2024-04-29 19:49:43 -0400

    Update task.py

--- myapp/task.py
+++ myapp/task.py
@@ -6,15 +6,15 @@ from .audio_processor import process_audio
 
 @shared_task
 def process_audio_task():
-    # 获取单个音频文件路径
+  
     latest_single_audio = SingleAudioFile.objects.latest("id")
     single_audio_path = latest_single_audio.file.path
 
-    # 获取所有音频文件路径
+
     all_audio_files = AudioFile.objects.all()
     folder_paths = [audio_file.file.path for audio_file in all_audio_files]
 
-    # 调用处理函数
+
     results = process_audio(single_audio_path, folder_paths)
 
     #  reserve for more process, store result to database
